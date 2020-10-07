5e583ff668c607160c638c3f48a1adea09a1ed9e
Lena <elenameln25033@gmail.com>
2020-10-07 20:37:52 +0300

    first commit

--- Circle.cs
+++ Circle.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace HW5__Melnychenko
 {
-    class Circle : Shape
+    internal class Circle : Shape
     {
-        public override string GetName => "Circle";
+        public string GetName => "Circle";
         public override void GetArea()
         {
             Console.WriteLine("Area is 25");
